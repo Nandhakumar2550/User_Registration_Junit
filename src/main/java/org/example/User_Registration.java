cee5d2f7c0b444a1c57997c9a0f1c516346270de
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-04-14 18:00:57 +0530

    clean commit

---
+++ src/main/java/org/example/User_Registration.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class User_Registration {
+
+    // UC1
+    public boolean validateFirstName(String firstName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$"; // define rule: first letter capital + min 3 chars
+        return Pattern.matches(regex, firstName); // check if input matches rule and return result
+    }
+}
---
+++ src/test/java/org/example/UserRegistrationTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserRegistrationTest {
+
+    User_Registration user = new User_Registration(); // create object
+
+    // UC1
+    @Test
+    void testFirstName() {
+        assertTrue(user.validateFirstName("Nandha")); // valid case
+        assertFalse(user.validateFirstName("na")); // invalid case
+    }
+}
